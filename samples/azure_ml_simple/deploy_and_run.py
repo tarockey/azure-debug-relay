871227369f5263c22df9ec7d1bf51e1b1a2be025
Vlad Kolesnikov <vladkol@microsoft.com>
2021-02-22 22:03:11 -0800

    fixed debugpy version

--- samples/azure_ml_simple/deploy_and_run.py
+++ samples/azure_ml_simple/deploy_and_run.py
@@ -90,7 +90,7 @@ conda_dependencies = run_config.environment.python.conda_dependencies
 conda_dependencies.add_conda_package("pip")
 conda_dependencies.add_pip_package("azureml-sdk==" + amlcore.__version__)
 conda_dependencies.add_pip_package("debugpy==1.2.1")
-conda_dependencies.add_pip_package("azure-debug-relay==0.3.0")
+conda_dependencies.add_pip_package("azure-debug-relay==0.3.1")
 
 train_step = PythonScriptStep(name='Train Step with Debugging',
                               script_name="samples/azure_ml_simple/steps/train.py",
